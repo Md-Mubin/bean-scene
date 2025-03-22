fe87a6ece1ad693f6e3f80949620608d260ef1d7
Md Mubin <fontdevmubin@gmail.com>
2025-03-22 23:29:12 +0600

    On branch master Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   postcss.config.mjs 	deleted:    public/file.svg 	deleted:    public/globe.svg 	new file:   public/images/Chai Latte.png 	new file:   public/images/Cuppuccino.png 	new file:   public/images/Expresso.png 	new file:   public/images/Macchiato.png 	new file:   public/images/Order_bg.png 	new file:   public/images/Subscribe_bg.png 	new file:   public/images/banner_bg.png 	new file:   public/images/beans.png 	new file:   public/images/coffe_cup.png 	new file:   public/images/coffee_bean.png 	new file:   public/images/coffee_blast.png 	new file:   public/images/fotter_bg.png 	new file:   public/images/reviewer.png 	new file:   public/images/side_coffee.png 	deleted:    public/next.svg 	deleted:    public/vercel.svg 	deleted:    public/window.svg 	deleted:    src/app/favicon.ico 	modified:   src/app/globals.css 	modified:   src/app/layout.js 	modified:   src/app/page.js 	new file:   src/components/Banner.jsx 	new file:   src/components/Discover.jsx 	new file:   src/components/Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='pt-5'>
+                <div className="container">
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
