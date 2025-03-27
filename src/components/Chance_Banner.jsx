d6b2f253b4d46441ca273228ffb3cfaaa19c0616
Md Mubin <fontdevmubin@gmail.com>
2025-03-27 23:55:32 +0600

    Changes to be committed: 	modified:   src/app/globals.css 	new file:   src/components/Chance_Banner.jsx

---
+++ src/components/Chance_Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Chance_Banner = () => {
+    return (
+        <>
+            <section className='chanceSection'>
+                <div className="container">
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Chance_Banner
