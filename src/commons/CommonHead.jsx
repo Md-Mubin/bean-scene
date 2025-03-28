8385bbdbc8ea8d3e419b0a385a4edfc23e5a613b
Md Mubin <fontdevmubin@gmail.com>
2025-03-28 23:45:11 +0600

    Changes to be committed: 	modified:   src/commons/CommonHead.jsx

--- src/commons/CommonHead.jsx
+++ src/commons/CommonHead.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CommonHead = ({ headName }) => {
     return (
         <>
-            <h2 className='text-center mt-[68px] mb-[29px] font-bold text-[54px] text-[#603809]'>
+            <h2 className='text-center mt-[68px] mb-[29px] font-bold text-3xl text-[#603809]' style={{fontSize : "calc(1vw + 40px)"}}>
                 {headName}
             </h2>
         </>
