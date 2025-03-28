7527295b768bb390bd92f1c5540bc3d725569f5e
Md Mubin <fontdevmubin@gmail.com>
2025-03-28 12:36:52 +0600

    new file:   src/app/head.js 	modified:   src/app/layout.js

---
+++ src/app/head.js
@@ -0,0 +1,11 @@
+export default function Head() {
+    return (
+        <>
+        <head>
+
+            <title>Bean Scene</title>
+            <meta name="description" content="Coffee Shop Coffee Beans Next.js React Next Web Developer Mubin Frontend Developer" />
+        </head>
+        </>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -6,14 +6,10 @@ import Footer from "@/components/Footer";
 export const brandFont = Nunito();
 export const logoFont = Clicker_Script({ weight: '400' });
 
-const metadata = {
-  title: "Bean Scene",
-  description: "Coffe Shop Coffe Beans Next.js react next web developer Mubin Frontend Developer",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>Bean Scene</title>
       <body className={brandFont.className}>
         <div className="sticky top-0 z-[100]">
           <Navbar />
